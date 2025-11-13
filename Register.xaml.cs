b560720f06a9de364b01c565fe9bb7944730d1e8
Nick Kisl <kisnik40@gmail.com>
2025-11-13 10:20:50 +0300

    передобавлена страница регистрации и добавлена кнопка "Войти"

---
+++ Register.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP_Project
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text.Trim();
+            string password = PasswordBox.Password;
+            string confirmPassword = ConfirmPasswordBox.Password;
+
+            // Проверка на заполненность
+            if (string.IsNullOrEmpty(username) ||
+                string.IsNullOrEmpty(password) ||
+                string.IsNullOrEmpty(confirmPassword))
+            {
+                MessageBox.Show("Заполните все поля.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            
+
+            // Проверка совпадения паролей
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Пароли не совпадают.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+
+            MessageBox.Show($"Регистрация прошла успешно!\nДобро пожаловать, {username}!",
+                            "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            // Очистка формы
+            UsernameBox.Clear();
+            PasswordBox.Clear();
+            ConfirmPasswordBox.Clear();
+        }
+    }
+}
