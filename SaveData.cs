d5b6bac097a39fb025576a5c03478777dff7b558
plier <plier@DESKTOP-1UNM33F>
2025-11-13 10:28:10 +0300

    класс сохранения данных

---
+++ SaveData.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.IO;
+
+namespace TP_Project
+{
+    static class SaveData
+    {
+        public static void Save(string data, string filepath)
+        {
+            string json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filepath, json);
+        }
+    }
+}
